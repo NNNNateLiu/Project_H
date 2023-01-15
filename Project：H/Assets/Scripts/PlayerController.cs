d1a2f9ce513652a1520489d36acdfc4dda5d4cec
GinLalvice <1062146264@qq.com>
2023-01-15 09:49:11 +0800

    Add 2F scene

--- Project：H/Assets/Scripts/PlayerController.cs
+++ Project：H/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     [Header("basic")]
     private Rigidbody2D rb2d;
     public float speed;
+    public Animator animator;
 
     [Header("status")]
     public GameObject canMove;
@@ -27,16 +28,26 @@ public class PlayerController : MonoBehaviour
         if (canMove.activeSelf is true)
         {
             rb2d.simulated = true;
-            speed = 1;
-            if(Input.GetKey(KeyCode.LeftShift )) //character running
+            speed = 0.5f ;
+
+            //character running
+            if (Input.GetKey(KeyCode.LeftShift ))
             {
-                speed = 1.5f;
+                speed = 1f;
             }
+
+            //activate animation
+            animator.enabled = true;
+
             Move();
         }
         else //freeze character during interaction
         {
             rb2d.simulated = false;
+            //disable animation
+            animator.enabled = false;
+
+
         }
     }
 
@@ -49,6 +60,71 @@ public class PlayerController : MonoBehaviour
         //Debug.Log("horizontalInput: " +horizontalInput + "  " + "verticalInput: " + verticalInput);
         Vector2 moveDirection = new Vector2(horizontalInput, verticalInput);
         rb2d.velocity = moveDirection * speed;
+
+        // move animation
+        //Left move
+        if (horizontalInput < 0)
+        {
+            animator.SetBool("Left", true);
+        }
+        else 
+        {
+            animator.SetBool("Left", false);
+        }
+
+        //Right move
+        if (horizontalInput > 0)
+        {
+            animator.SetBool("Right", true);
+        }
+        else
+        {
+            animator.SetBool("Right", false);
+        }
+
+        //Front move
+        if (verticalInput < 0)
+        {
+            animator.SetBool("Front", true);
+        }
+        else
+        {
+            animator.SetBool("Front", false);
+        }
+
+        //Back move
+        if (verticalInput > 0)
+        {
+            animator.SetBool("Back", true);
+        }
+        else
+        {
+            animator.SetBool("Back", false);
+        }
+
+
+        /*
+        //play back animation
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("Back", true); 
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("Back", false);
+        }
+
+
+        //play right animation
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("Right", true); 
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("Right", false);
+        }
+        */
     }
 }
 
